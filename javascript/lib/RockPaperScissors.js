712438ddd0e19066ade8701c30374e6e5286181f
James B Kemp <james@bibble.com>
2014-06-23 14:32:17 +0100

    just some lyout ahcnges

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,11 +1,3 @@
-function Player(name) {
-  this.name = name;
-};
-
-Player.prototype.picks = function(pick) {
-  this.pick = pick;
-};
-
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
@@ -15,4 +7,12 @@ Game.prototype.winner = function() {
   if(1 === 1) {
     return this.player1;
   }
+};
+
+function Player(name) {
+  this.name = name;
+};
+
+Player.prototype.picks = function(pick) {
+  this.pick = pick;
 };
